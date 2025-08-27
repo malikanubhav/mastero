051244bb1d1641fb05b306598d9ce07fc57d0860
Anubhav <121726561+malikanubhav@users.noreply.github.com>
2025-08-27 10:46:03 +0530

    Update questions.js

--- backend/src/routes/questions.js
+++ backend/src/routes/questions.js
@@ -2,11 +2,11 @@ import express from "express";
 import { auth } from "./middlewares/auth.js";
 import { rbac } from "./middlewares/rbac.js";
 import {createQuestion,listQuestions,getQuestion,updateQuestion,deleteQuestion} from "../controllers/questionsController.js";
-
+import { pagination } from "./middlewares/pagination.js";
 const router = express.Router();
 
 router.post("/", auth, rbac("admin"), createQuestion);
-router.get("/", auth, rbac("admin"), listQuestions);
+router.get("/", auth, rbac("admin"), pagination(10,100), listQuestions);
 router.get("/:id", auth, rbac("admin"), getQuestion);
 router.put("/:id", auth, rbac("admin"), updateQuestion);
 router.delete("/:id", auth, rbac("admin"), deleteQuestion);
